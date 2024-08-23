f991a563d5719ccec648645290ce1129030af205
rafiftrlh <rafifitro89@gmail.com>
2024-08-23 15:23:41 +0700

    feat: create staff

--- controllers/system_user.controller.js
+++ controllers/system_user.controller.js
@@ -1,4 +1,7 @@
 import supabase from "../services/supabase_client.js"
+import bcrypt from "bcrypt"
+
+const saltRounds = 10
 
 export const getAllSystemUser = async (req, res) => {
   try {
@@ -43,6 +46,31 @@ export const getAllStaff = async (req, res) => {
     console.error(`Error fetching user: ${error}`)
     res.status(500).json({
       msg: "Internal server error",
+      err: error.message
+    })
+  }
+}
+
+export const createStaff = async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body
+    const hashPassword = await bcrypt.hash(password, saltRounds)
+
+    if (role === "admin") {
+      return res.status(400).json({ msg: "Role 'admin' is not allowed for staff accounts." });
+    }
+
+    const newUser = await supabase.from("system_users").insert({
+      name,
+      email,
+      password: hashPassword,
+      role,
+    }).select()
+
+    res.status(201).json({ msg: `Created a staff account with the name ${name} successfully`, user: newUser })
+  } catch (error) {
+    res.status(400).json({
+      msg: `Failed to create a staff account`,
       err: error.message
     })
   }
--- routes/system_user.route.js
+++ routes/system_user.route.js
@@ -1,12 +1,12 @@
 import express from "express"
-import { getAllStaff, getAllSystemUser, getSystemUserById } from "../controllers/system_user.controller.js"
+import { createStaff, getAllStaff, getAllSystemUser, getSystemUserById } from "../controllers/system_user.controller.js"
 
 const router = express.Router()
 
 router.get("/", getAllSystemUser)
 router.get("/get-all-staff", getAllStaff)
 router.get("/:id", getSystemUserById)
-// router.post("/", createStaff)
+router.post("/", createStaff)
 // router.patch("/:id", updateStaff)
 // router.patch("/soft-delete/:id", softDeleteStaff)
 // router.patch("/restore/:id", restoreStaff)

5e2de430e5756f66418f4f589a54433c52252188
rafiftrlh <rafifitro89@gmail.com>
2024-08-23 15:09:07 +0700

    feat: get system users by id

--- controllers/system_user.controller.js
+++ controllers/system_user.controller.js
@@ -13,3 +13,23 @@ export const getAllSystemUser = async (req, res) => {
     })
   }
 }
+
+export const getSystemUserById = async (req, res) => {
+  try {
+    const { id } = req.params
+
+    const user = await supabase.from("system_users").select().eq("id", id).single()
+
+    if (!user) {
+      return res.status(404).json({ msg: "User not found" })
+    }
+
+    res.status(200).json(user.data)
+  } catch (error) {
+    console.error(`Error fetching user: ${error}`)
+    res.status(500).json({
+      msg: "Internal server error",
+      err: error.message
+    })
+  }
+}
--- routes/system_user.route.js
+++ routes/system_user.route.js
@@ -1,11 +1,11 @@
 import express from "express"
-import { getAllSystemUser } from "../controllers/system_user.controller.js"
+import { getAllSystemUser, getSystemUserById } from "../controllers/system_user.controller.js"
 
 const router = express.Router()
 
 router.get("/", getAllSystemUser)
 // router.get("/get-all-staff", getAllStaff)
-// router.get("/:id", getSystemUser)
+router.get("/:id", getSystemUserById)
 // router.post("/", createStaff)
 // router.patch("/:id", updateStaff)
 // router.patch("/soft-delete/:id", softDeleteStaff)
